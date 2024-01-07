7c6ce4844ec4aca0c295feba9ac18e5a0716d00a
Magomedali Gajiev <mag198421@gmail.com>
2024-01-07 15:11:47 +0300

    Добавил  класс Triangle

---
+++ Classes/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Triangle : ITest
+    {
+        public double a;
+        public string Name { get; set; }
+
+        public double Dioganal()
+        {
+            return a;
+        }
+    }
+}
