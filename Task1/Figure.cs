b1125e30feba7ded52dcbc251d2ac5edefc4656f
Magomedali Gajiev <mag198421@gmail.com>
2024-01-07 14:00:17 +0300

    Добавил классы Figure и Square

---
+++ Task1/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Figure
+    {
+        double a; // сторона фигуры
+
+        public double P()
+        {
+            return a;
+        }
+
+        public double S()
+        {
+            return 0;
+        }
+    }
+}
