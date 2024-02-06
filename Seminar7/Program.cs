d8879e05e7d161fca6b6b90ff64f9313be5a0e7a
Magomedali Gajiev <mag198421@gmail.com>
2024-02-06 12:25:45 +0300

    Добавлено ДЗ к семинару 7

---
+++ Seminar7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Seminar9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass originalObj = new TestClass(42, "Hello", 3.14m, new char[] { 'a', 'b', 'c' });
+
+            string savedData = ReflectionExample.SaveClassToString(originalObj);
+            Console.WriteLine(savedData);
+
+            TestClass restoredObj = ReflectionExample.LoadClassFromString(savedData);
+            Console.WriteLine($"I: {restoredObj.I}");
+            Console.WriteLine($"S: {restoredObj.S}");
+            Console.WriteLine($"D: {restoredObj.D}");
+            Console.WriteLine($"C: {string.Join(", ", restoredObj.C)}");
+        }    
+    }
+}
---
+++ Seminar7/ReflectionExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Seminar9
+{
+    public class ReflectionExample
+    {
+        public static string SaveClassToString(TestClass obj)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = obj.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                string propertyName = property.Name;
+                object propertyValue = property.GetValue(obj);
+
+                sb.AppendLine($"{propertyName}: {propertyValue}");
+            }
+
+            return sb.ToString();
+        }
+
+        public static TestClass LoadClassFromString(string data)
+        {
+            TestClass obj = new TestClass();
+
+            string[] lines = data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string propertyName = parts[0];
+                string propertyValue = parts[1];
+
+                PropertyInfo property = obj.GetType().GetProperty(propertyName);
+
+                if (property != null)
+                {
+                    Type propertyType = property.PropertyType;
+
+                    if (propertyType == typeof(int))
+                    {
+                        property.SetValue(obj, int.Parse(propertyValue));
+                    }
+                    else if (propertyType == typeof(string))
+                    {
+                        property.SetValue(obj, propertyValue);
+                    }
+                    else if (propertyType == typeof(decimal))
+                    {
+                        property.SetValue(obj, decimal.Parse(propertyValue));
+                    }
+                    else if (propertyType == typeof(char[]))
+                    {
+                        property.SetValue(obj, propertyValue.ToCharArray());
+                    }
+                }
+            }
+
+            return obj;
+        }
+    }
+}
