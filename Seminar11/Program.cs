3eb46fcf104aa665b9681e6e25ddf36e2bd87c1a
Magomedali Gajiev <mag198421@gmail.com>
2024-02-08 09:42:52 +0300

    Добавлен Семинар9

---
+++ Seminar11/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Seminar11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Weather
+        {
+            WeatherInfo current;
+            List<WeatherInfo> history;
+
+            class WeatherInfo : IComparable<WeatherInfo> 
+            {
+                DateTime time;
+                int teperature;
+                int weathecode;
+                double windspeed;
+                int winddirection;
+
+                public int CompareTo(WeatherInfo? other)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        
+    }
+}
